9e7d3d125eefe7a5fd264ba1736b5210b2eee368
FRED KANEZA <95358548+amankanz@users.noreply.github.com>
2024-08-17 13:50:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const initialTip = 'Press "Get advice" to be advised...🌟';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Main />
+    </>
+  );
+}
+
+function Main() {
+  return (
+    <main id="container">
+      <Heading />
+      <AdviceContainer />
+      <Logo />
+    </main>
+  );
+}
+
+function Heading() {
+  return <h2>Advise Me 👴🏽</h2>;
+}
+
+function Logo() {
+  return <img src="/1.png" alt="ClickWisdom logo" id="adviseMeImg" />;
+}
+
+function AdviceContainer() {
+  const [loader, setLoader] = useState(false);
+  const [loadedAdvice, setLoadedAdvice] = useState(initialTip);
+
+  function loading() {
+    setLoader(true);
+    setLoadedAdvice(false);
+  }
+
+  function completeLoading() {
+    setLoader(false);
+  }
+
+  async function handleGetAdvice() {
+    loading();
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice", {
+        header: {
+          Accept: "application/json",
+        },
+      });
+
+      const adviceData = await response.json();
+
+      setLoadedAdvice(adviceData.slip.advice);
+      completeLoading();
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }
+
+  return (
+    <section className="advice-container">
+      <Advice initialTip={loadedAdvice} />
+      <Loader loader={loader} />
+      <Button onGetAdvice={handleGetAdvice} />
+    </section>
+  );
+}
+
+function Advice({ initialTip }) {
+  return (
+    <div className="advice">
+      <FontAwesome />
+      <LoadingAdvice>{initialTip}</LoadingAdvice>
     </div>
   );
 }
 
-export default App;
+function FontAwesome() {
+  return <i className="fa-sharp fa-solid fa-quote-left"></i>;
+}
+
+function LoadingAdvice({ children }) {
+  return (
+    <p
+      id="loadingAdvice"
+      style={children ? { display: "block" } : { display: "none" }}
+    >
+      {children}
+    </p>
+  );
+}
+
+function Loader({ loader }) {
+  return (
+    <div
+      className="loader"
+      style={loader ? { display: "block" } : { display: "none" }}
+    ></div>
+  );
+}
+
+function Button({ onGetAdvice }) {
+  return (
+    <button id="loadAdviceBtn" onClick={onGetAdvice}>
+      Get Advice
+    </button>
+  );
+}
